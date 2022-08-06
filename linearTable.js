7c519d61685173d556565c0081873d0626d56398
fabio-sv <fabio_dev@outlook.com>
2022-08-06 18:25:43 -0300

    update

---
+++ linearTable.js
@@ -0,0 +1,19 @@
+// Insere um novo valor no final da tabela linear
+function insertLinearTable(array, value) {
+	// Crie um tempArray maior que array 
+	const tempArray = new Array(array.length + 1);
+
+	for(let i = 0; i < array.length; i++) {
+		// Copie os valores de array para tempArray
+		tempArray[i] = array[i]
+	}
+	// Insira o valor no ultimo indice do tempArray
+	tempArray[array.length] = value;
+
+	return tempArray;
+}
+
+const scores = new Array(90, 70, 50, 80, 60, 85);
+
+console.log(insertLinearTable(scores, 75)); // [90, 70, 50, 80,60, 85, 75]
+
